dbf0144955aac3923dd56f379428a4164a939a22
vinzmobile1 <salesvinzmobile1@gmail.com>
2025-02-19 10:26:01 +0700

    Update app1.py

--- app1.py
+++ app1.py
@@ -7,8 +7,8 @@ import os
 
 # Data user dan password beserta masa aktifnya
 users = {
-    "user1": {"password": "@123User1", "valid_until": "18/02/2025"},
-    "user2": {"password": "@123User2", "valid_until": "19/02/2025"}
+    "user1": {"password": "@User1", "valid_until": "20/02/2025"},
+    "user2": {"password": "@User2", "valid_until": "25/02/2025"}
 }
 
 # Fungsi untuk memeriksa otentikasi
